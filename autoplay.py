e0bd0c72763c3cfc78f9600f5184b0300a95b851
Mateus Macedo <84480587+macedocedo@users.noreply.github.com>
2024-01-18 15:18:48 -0300

    Update autoplay.py

--- autoplay.py
+++ autoplay.py
@@ -5,19 +5,9 @@ with sync_playwright() as p:
     browser = p.chromium.launch(headless=False) #headless=False comando roda sem visualizar
     page = browser.new_page() #Abre um navegador
     page.goto("https://google.com") #Link
-    page.fill("input[name='q']",'tesssste') #Inputar informação
-    page.click("button['btnK']") #selecionar botão
-    page.wait_for_timeout(100000) #tempo que app ficará aberta
-    print(page.title()) #Mostra a tela
+    page.locator('xpath=//*[@id="APjFqb"]').click() #click na busca
+    page.fill('xpath=//*[@id="APjFqb"]',"teste") #Texto que será preenchido
+    page.keyboard.press('Enter') #Enter 
+    page.wait_for_timeout(50000) #tempo que app ficará aberta
     browser.close() #fechar
-
-
-    #Fechar navegador
-    #browser.close()
-
-    #Input de informação
-    #page.fill("input[name='q']",'texto')
-
-    #input de click
-    #page.click("button['infodobotão']")
-    #page.click("input['infodobotão']")
+  
